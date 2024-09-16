310dbaef7a5b311f7b62212da7435f236a6ab6b9
gabriel silva dos anjos <69930494+gabriel-anjos@users.noreply.github.com>
2024-09-16 15:43:16 -0300

    Update ClientePubSub.ino

--- ClientePubSub/ClientePubSub.ino
+++ ClientePubSub/ClientePubSub.ino
@@ -1,187 +1,74 @@
-#include <WiFi.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h> 
-#include <time.h>
-#define emptyString String()
+import paho.mqtt.client as paho
+import os
+import socket
+import ssl
+from time import sleep
+from random import uniform
+import json
 
-//Enter values in secrets.h ▼
-#include "secrets.h"
+import logging
+logging.basicConfig(level=logging.INFO)
 
-const int MQTT_PORT = 8883;
-const char MQTT_SUB_TOPIC[] = "$aws/things/" THINGNAME "/shadow/update";
-const char MQTT_PUB_TOPIC[] = "$aws/things/" THINGNAME "/shadow/update";
+class PubSub(object):
 
-#ifdef USE_SUMMER_TIME_DST
-uint8_t DST = 1;
-#else
-uint8_t DST = 0;
-#endif
+    def __init__(self, listener = False, topic = "default"):
+        self.connect = False
+        self.listener = listener
+        self.topic = topic
+        self.logger = logging.getLogger(repr(self))
 
-WiFiClientSecure net;
+    def __on_connect(self, client, userdata, flags, rc):
+        self.connect = True
+        
+        if self.listener:
+            self.mqttc.subscribe(self.topic)
 
-BearSSL::X509List cert(cacert);
-BearSSL::X509List client_crt(client_cert);
-BearSSL::PrivateKey key(privkey)
+        self.logger.debug("{0}".format(rc))
 
-PubSubClient client(net);
+    def __on_message(self, client, userdata, msg):
+        self.logger.info("{0}, {1} - {2}".format(userdata, msg.topic, msg.payload))
 
-unsigned long lastMillis = 0;
-time_t now;
-time_t nowish = 1510592825;
+    def __on_log(self, client, userdata, level, buf):
+        self.logger.debug("{0}, {1}, {2}, {3}".format(client, userdata, level, buf))
 
-void NTPConnect(void)
-{
-  Serial.print("Setting time using SNTP");
-  configTime(TIME_ZONE * 3600, DST * 3600, "pool.ntp.org", "time.nist.gov");
-  now = time(nullptr);
-  while (now < nowish)
-  {
-    delay(500);
-    Serial.print(".");
-    now = time(nullptr);
-  }
-  Serial.println("done!");
-  struct tm timeinfo;
-  gmtime_r(&now, &timeinfo);
-  Serial.print("Current time: ");
-  Serial.print(asctime(&timeinfo));
-}
+    def bootstrap_mqtt(self):
 
-void messageReceived(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Received [");
-  Serial.print(topic);
-  Serial.print("]: ");
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
+        self.mqttc = paho.Client()
+        self.mqttc.on_connect = self.__on_connect
+        self.mqttc.on_message = self.__on_message
+        self.mqttc.on_log = self.__on_log
 
-void pubSubErr()
-{
-    Serial.print("Connection lost");
-}
+        awshost = "yourthingendpointname.iot.us-east-1.amazonaws.com"
+        awsport = 8883
 
-void connectToMqtt(bool nonBlocking = false)
-{
-  Serial.print("MQTT connecting ");
-  while (!client.connected())
-  {
-    if (client.connect(THINGNAME))
-    {
-      Serial.println("connected!");
-      if (!client.subscribe(MQTT_SUB_TOPIC))
-        pubSubErr();
-    }
-    else
-    {
-      Serial.print("failed, reason -> ");
-      pubSubErr();
-      if (!nonBlocking)
-      {
-        Serial.println(" < try again in 5 seconds");
-        delay(5000);
-      }
-      else
-      {
-        Serial.println(" <");
-      }
-    }
-    if (nonBlocking)
-      break;
-  }
-}
+        caPath = "./authority.pem" # Root certificate authority, comes from AWS with a long, long name
+        certPath = "./2bafa20887-certificate.pem.crt"
+        keyPath = "./2bafa20887-private.pem.key"
 
-void connectToWiFi(String init_str)
-{
-  if (init_str != emptyString)
-    Serial.print(init_str);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  if (init_str != emptyString)
-    Serial.println("ok!");
-}
+        self.mqttc.tls_set(caPath, 
+            certfile=certPath, 
+            keyfile=keyPath, 
+            cert_reqs=ssl.CERT_REQUIRED, 
+            tls_version=ssl.PROTOCOL_TLSv1_2, 
+            ciphers=None)
 
-void checkWiFiThenMQTT(void)
-{
-  connectToWiFi("Checking WiFi");
-  connectToMqtt();
-}
+        result_of_connection = self.mqttc.connect(awshost, awsport, keepalive=120)
 
-unsigned long previousMillis = 0;
-const long interval = 5000;
+        if result_of_connection == 0:
+            self.connect = True
 
-void checkWiFiThenMQTTNonBlocking(void)
-{
-  connectToWiFi(emptyString);
-  if (millis() - previousMillis >= interval && !client.connected()) {
-    previousMillis = millis();
-    connectToMqtt(true);
-  }
-}
+        return self
 
-void checkWiFiThenReboot(void)
-{
-  connectToWiFi("Checking WiFi");
-  Serial.print("Rebooting");
-  ESP.restart();
-}
+    def start(self):
+        self.mqttc.loop_start()
 
-void sendData(void)
-{
-  DynamicJsonDocument jsonBuffer(JSON_OBJECT_SIZE(3) + 100);
-  JsonObject root = jsonBuffer.to<JsonObject>();
-  JsonObject state = root.createNestedObject("state");
-  JsonObject state_reported = state.createNestedObject("reported");
-  state_reported["value"] = random(100);
-  Serial.printf("Sending  [%s]: ", MQTT_PUB_TOPIC);
-  serializeJson(root, Serial);
-  Serial.println();
-  char shadow[measureJson(root) + 1];
-  serializeJson(root, shadow, sizeof(shadow));
-  if (!client.publish(MQTT_PUB_TOPIC, shadow, false))
-    pubSubErr(client.state());
-}
+        while True:
+            sleep(2)
+            if self.connect == True:
+                self.mqttc.publish(self.topic, json.dumps({"message": "Hello COMP680"}), qos=1)
+            else:
+                self.logger.debug("Attempting to connect.")
 
-void setup()
-{
-  Serial.begin(115200);
-  delay(5000);
-  Serial.println();
-  Serial.println();
-  WiFi.hostname(THINGNAME);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  connectToWiFi(String("Attempting to connect to SSID: ") + String(ssid));
-  NTPConnect();
-  net.setTrustAnchors(&cert);
-  net.setClientRSACert(&client_crt, &key);
-  client.setServer(MQTT_HOST, MQTT_PORT);
-  client.setCallback(messageReceived);
-  connectToMqtt();
-}
-
-void loop()
-{
-  now = time(nullptr);
-  if (!client.connected())
-  {
-    checkWiFiThenMQTT();
-  }
-  else
-  {
-    client.loop();
-    if (millis() - lastMillis > 5000)
-    {
-      lastMillis = millis();
-      sendData();
-    }
-  }
-}
+if __name__ == '__main__':
+    
+    PubSub(listener = True, topic = "chat-evets").bootstrap_mqtt().start()
